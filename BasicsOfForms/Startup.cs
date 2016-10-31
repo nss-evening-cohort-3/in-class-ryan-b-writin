a279194f4b7893d1d32dfa9e1ecb1a525d3c8db4
Jurnell Cockhren <jurnell@sophicware.com>
2016-10-31 16:48:08 -0700

    Initial Commit

---
+++ BasicsOfForms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BasicsOfForms.Startup))]
+namespace BasicsOfForms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
