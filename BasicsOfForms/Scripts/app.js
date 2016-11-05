00896e21bbd2b6fb3625c189648784ffbdb2aabd
Ryan B <admirallasers@gmail.com>
2016-11-05 10:11:32 -0500

    complete

--- BasicsOfForms/Scripts/app.js
+++ BasicsOfForms/Scripts/app.js
@@ -1,5 +1,32 @@
 ﻿var app = angular.module("formBasics", []);
 
-app.controller("BasicsCtrl", ["$scope", function($scope){
-    $scope.yes = "yes";
-}])
+app.controller("FormCtrl", function ($scope, $http) {
+
+    $scope.user = {
+        email: "",
+        password: "",
+        confirm: "",
+    };
+
+    $scope.formSubmit = function (event) {
+        console.log("clicked!");
+
+        console.log($("#angularForm > input"));
+        $scope.user.email = $("#angularForm input[name=email]").val();
+        $scope.user.password = $("#angularForm input[name=password]").val();
+        $scope.user.confirm = $("#angularForm  input[name=confirm]").val();
+
+        $http({
+            method: "POST",
+            url: "/api/AngularApi",
+            data: $scope.user
+        }).then(function (response) {
+            console.log("it workded");
+            console.log(JSON.stringify($scope.user))
+        }, function (error) {
+            console.log("failed");
+        });
+
+        event.preventDefault();
+    };
+})
